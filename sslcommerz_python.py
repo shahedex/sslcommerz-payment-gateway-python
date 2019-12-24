a476a07db943f8f498a16d17a4144eb20219bc75
Shahed Mehbub <shahed739@gmail.com>
2019-12-24 18:00:23 +0600

    added: set integration data and update README

--- sslcommerz_python.py
+++ sslcommerz_python.py
@@ -1,4 +1,6 @@
 from typing import Dict
+from decimal import Decimal
+from uuid import uuid4
 
 import _constants as const
 
@@ -8,6 +10,7 @@ class SSLCPayment:
     sslc_store_id : str
     sslc_store_pass : str
     sslc_mode_name : str
+    integration_data : Dict[str, str] = {}
 
     def __init__(self, sslc_is_sandbox=True, sslc_store_id='', sslc_store_pass='') -> None:
         self.sslc_mode_name = self.set_sslcommerz_mode(sslc_is_sandbox)
@@ -23,3 +26,19 @@ class SSLCPayment:
             return 'sandbox'
         else:
             return 'securepay'
+
+    def set_user_info(self):
+        pass
+
+    def set_integration(self, total_amount: Decimal, currency: str, product_category: str,
+                        success_url: str, fail_url: str, cancel_url: str, ipn_url: str='') -> None:
+        self.integration_data['store_id'] = self.sslc_store_id
+        self.integration_data['store_passwd'] = self.sslc_store_pass
+        self.integration_data['tran_id'] = str(uuid4())
+        self.integration_data['total_amount'] = total_amount
+        self.integration_data['currency'] = currency
+        self.integration_data['product_category'] = product_category
+        self.integration_data['success_url'] = success_url
+        self.integration_data['fail_url'] = fail_url
+        self.integration_data['cancel_url'] = cancel_url
+        self.integration_data['ipn_url'] = ipn_url
