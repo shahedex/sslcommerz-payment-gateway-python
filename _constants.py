6814bce46740e6b19cde7a86509a8ccf94267177
Shahed Mehbub <bumblebee@Shaheds-MacBook-Pro.local>
2019-12-24 15:42:41 +0600

    added: sslcpayment init and constants

---
+++ _constants.py
@@ -0,0 +1,2 @@
+SSLCZ_SESSION_API = 'sslcommerz.com/gwprocess/v4/api.php'
+SSLCZ_VALIDATION_API = 'sslcommerz.com/validator/api/validationserverAPI.php'
---
+++ sslcommerz_python.py
@@ -0,0 +1,25 @@
+from typing import Dict
+
+import _constants as const
+
+
+class SSLCPayment:
+    sslc_is_sandbox : bool
+    sslc_store_id : str
+    sslc_store_pass : str
+    sslc_mode_name : str
+
+    def __init__(self, sslc_is_sandbox=True, sslc_store_id='', sslc_store_pass='') -> None:
+        self.sslc_mode_name = self.set_sslcommerz_mode(sslc_is_sandbox)
+        self.sslc_is_sandbox = sslc_is_sandbox
+        self.sslc_store_id = sslc_store_id
+        self.sslc_store_pass = sslc_store_pass
+        self.sslc_session_api = 'https://' + self.sslc_mode_name + '.' + const.SSLCZ_SESSION_API
+        self.sslc_validation_api = 'https://' + self.sslc_mode_name + '.' + const.SSLCZ_VALIDATION_API
+
+
+    def set_sslcommerz_mode(self, sslc_is_sandbox: bool) -> str:
+        if sslc_is_sandbox is True or sslc_is_sandbox == 1:
+            return 'sandbox'
+        else:
+            return 'securepay'
